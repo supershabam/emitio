db80bd21d7289d8a92f64374cfd2ac11b3beca21
Ian Hansen <ian@supershabam.com>
2018-02-01 19:42:37 -0700

    testing makes the world go round

--- emitio/pkg/transformers/js_test.go
+++ emitio/pkg/transformers/js_test.go
@@ -8,12 +8,93 @@ import (
 )
 
 func TestJS(t *testing.T) {
-	js, err := NewJS(`function transform(acc, lines) {
+	tt := []struct {
+		name     string
+		program  string
+		inacc    []string
+		inlines  [][]string
+		outacc   []string
+		outlines [][]string
+		outerr   []error
+		err      error
+	}{
+		{
+			name: "passthrough",
+			program: `
+function transform(acc, lines) {
 	return [acc, lines]
-}`)
-	require.Nil(t, err)
-	acc, lines, err := js.Transform("hi", []string{"one", "two"})
-	require.Nil(t, err)
-	assert.Equal(t, "hi", acc)
-	assert.Equal(t, []string{"one", "two"}, lines)
+}`,
+			inacc: []string{
+				"acc",
+				"",
+				"世界",
+			},
+			inlines: [][]string{
+				{"one", "two"},
+				{},
+				{"世界世界"},
+			},
+			outacc: []string{
+				"acc",
+				"",
+				"世界",
+			},
+			outlines: [][]string{
+				{"one", "two"},
+				{},
+				{"世界世界"},
+			},
+			outerr: []error{nil, nil, nil},
+			err:    nil,
+		},
+		{
+			name: "double",
+			program: `
+function transform(acc, lines) {
+	return [acc + acc, [].concat(lines).concat(lines)]
+}`,
+			inacc: []string{
+				"acc",
+				"",
+				"世界",
+			},
+			inlines: [][]string{
+				{"one", "two"},
+				{},
+				{"世界世界"},
+			},
+			outacc: []string{
+				"accacc",
+				"",
+				"世界世界",
+			},
+			outlines: [][]string{
+				{"one", "two", "one", "two"},
+				{},
+				{"世界世界", "世界世界"},
+			},
+			outerr: []error{nil, nil, nil},
+			err:    nil,
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			js, err := NewJS(tc.program)
+			if tc.err != nil {
+				require.NotNil(t, err)
+				assert.Contains(t, err.Error(), tc.err.Error())
+				return
+			}
+			for idx := range tc.inacc {
+				acc, lines, err := js.Transform(tc.inacc[idx], tc.inlines[idx])
+				if err != nil {
+					require.NotNil(t, tc.outerr[idx])
+					assert.Contains(t, err.Error(), tc.outerr[idx].Error())
+					continue
+				}
+				assert.Equal(t, tc.outacc[idx], acc)
+				assert.Equal(t, tc.outlines[idx], lines)
+			}
+		})
+	}
 }
