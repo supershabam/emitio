b5d48a6cee8310f442c8858a27949d2dc441d63a
Ian Hansen <ian@supershabam.com>
2018-02-27 02:41:03 -0700

    streaming results

--- boson/column.go
+++ boson/column.go
@@ -2,6 +2,9 @@ package boson
 
 import (
 	"context"
+	"fmt"
+	"math"
+	"sync"
 	"time"
 
 	"golang.org/x/sync/errgroup"
@@ -17,24 +20,131 @@ type StringColumn struct {
 	Values []string
 }
 
-type Page struct {
-	Index          []time.Time
-	Float64Columns map[string]Float64Column
-	StringColumns  map[string]StringColumn
+type Wait func() error
+
+func sequence(filters ...filter) filter {
+	return func(ctx context.Context, d *Database, in <-chan []uint32) (<-chan []uint32, Wait) {
+		var wait func() error
+		for _, f := range filters {
+			in, wait = f(ctx, d, in)
+		}
+		return in, wait
+	}
 }
 
-type Predi interface {
-	Key() string
-	MatchFloat64(float64) bool
-	MatchString(string) bool
+type filter func(ctx context.Context, d *Database, in <-chan []uint32) (<-chan []uint32, Wait)
+
+type Database struct {
+	index          []time.Time
+	float64Columns map[string]Float64Column
+	stringColumns  map[string]StringColumn
+	l              sync.Mutex
 }
 
-type matchIDs func(acc, local []uint32, values []float64) []uint32
+func (d *Database) Float64Columns(source, field string, ids []uint32) ([]uint32, []float64, error) {
+	var columns []uint32
+	var values []float64
+	d.l.Lock()
+	l, ok := d.float64Columns[field]
+	d.l.Unlock()
+	if !ok {
+		fmt.Printf("tried to find field=%s but did not\n", field)
+		return columns, values, nil
+	}
+	columns, values = make([]uint32, 0, len(ids)), make([]float64, 0, len(ids))
+	idx := 0
+	cdx := 0
+	for idx < len(ids) && cdx < len(l.Index) {
+		id, col := ids[idx], l.Index[cdx]
+		if id == col {
+			columns = append(columns, col)
+			values = append(values, l.Values[cdx])
+			cdx++
+			idx++
+			continue
+		}
+		if id < col {
+			idx++
+			continue
+		}
+		cdx++
+	}
+	return columns, values, nil
+}
 
-type filter func(ctx context.Context, in <-chan []uint32) (<-chan []uint32, Wait)
+func (d *Database) StringColumns(source, field string, ids []uint32) ([]uint32, []string, error) {
+	var columns []uint32
+	var values []string
+	d.l.Lock()
+	l, ok := d.stringColumns[field]
+	d.l.Unlock()
+	if !ok {
+		return columns, values, nil
+	}
+	columns, values = make([]uint32, 0, len(ids)), make([]string, 0, len(ids))
+	idx := 0
+	cdx := 0
+	for idx < len(ids) && cdx < len(l.Index) {
+		id, col := ids[idx], l.Index[cdx]
+		if id == col {
+			columns = append(columns, col)
+			values = append(values, l.Values[cdx])
+			cdx++
+			idx++
+			continue
+		}
+		if id < col {
+			idx++
+			continue
+		}
+		cdx++
+	}
+	return columns, values, nil
+}
+
+func between(ctx context.Context, d *Database, start, end time.Time) (<-chan []uint32, Wait) {
+	ch := make(chan []uint32)
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		defer close(ch)
+		batch := make([]uint32, 0, 512)
+		flush := func() bool {
+			if len(batch) == 0 {
+				return true
+			}
+			select {
+			case <-ctx.Done():
+				return false
+			case ch <- batch:
+				batch = make([]uint32, 0, 512)
+				return true
+			}
+		}
+		for idx := 0; idx < len(d.index); idx++ {
+			at := d.index[idx]
+			if at.Before(start) {
+				continue
+			}
+			if at.Equal(end) || at.After(end) {
+				flush()
+				return nil
+			}
+			batch = append(batch, uint32(idx))
+			if len(batch) == cap(batch) {
+				ok := flush()
+				if !ok {
+					return nil
+				}
+			}
+		}
+		flush()
+		return nil
+	})
+	return ch, eg.Wait
+}
 
 func makeFloat64ColumnFilter(source, field string, match func(id, column uint32, value float64) bool) filter {
-	return func(ctx context.Context, d Database, in <-chan []uint32) (<-chan []uint32, Wait) {
+	return func(ctx context.Context, d *Database, in <-chan []uint32) (<-chan []uint32, Wait) {
 		ch := make(chan []uint32)
 		eg, ctx := errgroup.WithContext(ctx)
 		eg.Go(func() error {
@@ -51,17 +161,68 @@ func makeFloat64ColumnFilter(source, field string, match func(id, column uint32,
 						continue
 					}
 					next := make([]uint32, 0, len(ids))
-					columns, values := d.Float64Columns(source, field, ids)
-					for idx, cdx := 0; idx < len(ids) && cdx < len(columns); idx++ {
+					columns, values, err := d.Float64Columns(source, field, ids)
+					if err != nil {
+						return err
+					}
+					for idx, cdx := 0, 0; idx < len(ids) && cdx < len(columns); idx++ {
 						id := ids[idx]
 						column := columns[cdx]
 						if id != column {
-							if match(id, 0, 0) {
+							if match(id, math.MaxUint32, 0) {
 								next = append(next, id)
 							}
 							continue
 						}
-						if match(id, column, value[cdx]) {
+						if match(id, column, values[cdx]) {
+							next = append(next, id)
+						}
+						cdx++
+					}
+					select {
+					case <-ctx.Done():
+						return nil
+					case ch <- next:
+					}
+				}
+			}
+		})
+		return ch, eg.Wait
+	}
+}
+
+func makeStringColumnFilter(source, field string, match func(id, column uint32, value string) bool) filter {
+	return func(ctx context.Context, d *Database, in <-chan []uint32) (<-chan []uint32, Wait) {
+		ch := make(chan []uint32)
+		eg, ctx := errgroup.WithContext(ctx)
+		eg.Go(func() error {
+			defer close(ch)
+			for {
+				select {
+				case <-ctx.Done():
+					return nil
+				case ids, active := <-in:
+					if !active {
+						return nil
+					}
+					if len(ids) == 0 {
+						continue
+					}
+					next := make([]uint32, 0, len(ids))
+					columns, values, err := d.StringColumns(source, field, ids)
+					if err != nil {
+						return err
+					}
+					for idx, cdx := 0, 0; idx < len(ids) && cdx < len(columns); idx++ {
+						id := ids[idx]
+						column := columns[cdx]
+						if id != column {
+							if match(id, math.MaxUint32, "") {
+								next = append(next, id)
+							}
+							continue
+						}
+						if match(id, column, values[cdx]) {
 							next = append(next, id)
 						}
 						cdx++
---
+++ boson/column_test.go
@@ -0,0 +1,126 @@
+package boson
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"sort"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func loadscots() (*Database, error) {
+	b, err := ioutil.ReadFile("./testdata/scotts.json")
+	if err != nil {
+		return nil, err
+	}
+	type scotts []struct {
+		ID      float64 `json:"ID"`
+		Date    string  `json:"Date"`
+		Title   string  `json:"Title"`
+		Sponsor string  `json:"Sponsor"`
+	}
+	var s scotts
+	err = json.Unmarshal(b, &s)
+	if err != nil {
+		return nil, err
+	}
+	sort.Slice(s, func(i, j int) bool {
+		it, err := time.Parse("2006-01-02T15:04:05", s[i].Date)
+		if err != nil {
+			panic(err)
+		}
+		jt, err := time.Parse("2006-01-02T15:04:05", s[j].Date)
+		if err != nil {
+			panic(err)
+		}
+		return it.Before(jt)
+	})
+	d := &Database{
+		index:          make([]time.Time, 0, len(s)),
+		float64Columns: map[string]Float64Column{},
+		stringColumns:  map[string]StringColumn{},
+	}
+	ids := Float64Column{
+		Index:  make([]uint32, 0, len(s)),
+		Values: make([]float64, 0, len(s)),
+	}
+	titles := StringColumn{
+		Index:  make([]uint32, 0, len(s)),
+		Values: make([]string, 0, len(s)),
+	}
+	sponsors := StringColumn{
+		Index:  make([]uint32, 0, len(s)),
+		Values: make([]string, 0, len(s)),
+	}
+	for idx, e := range s {
+		t, err := time.Parse("2006-01-02T15:04:05", e.Date)
+		if err != nil {
+			return nil, err
+		}
+		d.index = append(d.index, t)
+		ids.Index = append(ids.Index, uint32(idx))
+		titles.Index = append(titles.Index, uint32(idx))
+		sponsors.Index = append(sponsors.Index, uint32(idx))
+		ids.Values = append(ids.Values, e.ID)
+		titles.Values = append(titles.Values, e.Title)
+		sponsors.Values = append(sponsors.Values, e.Sponsor)
+	}
+	d.float64Columns["ID"] = ids
+	d.stringColumns["Title"] = titles
+	d.stringColumns["Sponsor"] = sponsors
+	return d, nil
+}
+
+func BenchmarkColumn(b *testing.B) {
+	ctx := context.Background()
+	f1 := makeStringColumnFilter("src", "Title", func(id, column uint32, value string) bool {
+		return column != math.MaxUint32 && strings.Contains(value, "Human")
+	})
+	f2 := makeStringColumnFilter("src", "Sponsor", func(id, column uint32, value string) bool {
+		return column != math.MaxUint32 && strings.Contains(value, "Bob")
+	})
+	f := sequence(f1, f2)
+	d, err := loadscots()
+	require.Nil(b, err)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		count := 0
+		in, _ := between(ctx, d, time.Now().Add(-time.Hour*24*365*2), time.Now())
+		nextCh, wait := f(ctx, d, in)
+		for next := range nextCh {
+			count += len(next)
+		}
+		err = wait()
+		require.Nil(b, err)
+	}
+}
+
+func TestColumn(t *testing.T) {
+	ctx := context.Background()
+	f1 := makeStringColumnFilter("src", "Title", func(id, column uint32, value string) bool {
+		return column != math.MaxUint32 && strings.Contains(value, "a")
+	})
+	f2 := makeStringColumnFilter("src", "Sponsor", func(id, column uint32, value string) bool {
+		return column != math.MaxUint32 && strings.Contains(value, "Bob")
+	})
+	f := sequence(f1, f2)
+	d, err := loadscots()
+	require.Nil(t, err)
+	count := 0
+	in, _ := between(ctx, d, time.Now().Add(-time.Hour*24*365*2), time.Now())
+	nextCh, wait := f(ctx, d, in)
+	for next := range nextCh {
+		count += len(next)
+	}
+	err = wait()
+	require.Nil(t, err)
+	fmt.Printf("count=%d\n", count)
+	assert.True(t, false)
+}
