1dab9015631f3aebe1af10b97ac32551396853be
Ian Hansen <ian@supershabam.com>
2018-04-07 02:15:12 -0600

    contextified

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -32,6 +32,8 @@ import {
   shareReplay
 } from "rxjs/operators";
 import { ajax } from "rxjs/ajax";
+import Button from "material-ui/Button";
+import Drawer from "material-ui/Drawer";
 
 interface LoginRequest {
   kind: "LoginRequest";
@@ -54,6 +56,9 @@ interface Logout {
 }
 
 interface State {
+  drawer: {
+    open: boolean;
+  };
   username?: string;
 }
 
@@ -76,23 +81,22 @@ function affector(
     case "LoginRequest":
       return [
         s,
-        empty()
-        // of<Action>({ kind: "LoginSuccess", username: a.username }).pipe(
-        //   delay(1500),
-        //   takeUntil(
-        //     a$.pipe(
-        //       filter(a => {
-        //         switch (a.kind) {
-        //           case "LoginSuccess":
-        //           case "LoginRequest":
-        //           case "Logout":
-        //             return true;
-        //         }
-        //         return false;
-        //       })
-        //     )
-        //   )
-        // )
+        of<Action>({ kind: "LoginSuccess", username: a.username }).pipe(
+          delay(1500),
+          takeUntil(
+            a$.pipe(
+              filter(a => {
+                switch (a.kind) {
+                  case "LoginSuccess":
+                  case "LoginRequest":
+                  case "Logout":
+                    return true;
+                }
+                return false;
+              })
+            )
+          )
+        )
       ];
     case "LoginSuccess":
       return [{ ...s, username: a.username }, empty()];
@@ -108,15 +112,10 @@ const affect = (
   action$$: Subject<Observable<Action>>
 ): Observable<State> => {
   const state$ = new Observable(observer => {
-    console.log("creating observable");
     let current = init;
     const action$ = action$$.pipe(flatMap(a$ => a$));
-    observer.next(current);
     const sub = action$
       .pipe(
-        tap(a => {
-          console.log("running action", a);
-        }),
         map(a => {
           return affector(current, a, state$, action$);
         })
@@ -127,120 +126,68 @@ const affect = (
         action$$.next(tuple[1]);
       });
     return () => {
-      console.log("unsubscribing");
       sub.unsubscribe();
     };
   });
   return state$.pipe(publishBehavior(init), refCount());
 };
 
-// const action$$ = new Subject<Observable<Action>>();
-// const state$ = affect(affector, {}, action$$);
-
-interface AppProps {
-  affector: any;
-  init: State;
-  action$: Observable<Action>;
-}
-
-class Heatmap extends React.Component<null, null> {
-  svg: any;
-
-  constructor(props) {
-    super(props);
-    this.svg = React.createRef();
+const init: State = {
+  drawer: {
+    open: false
   }
+};
+const action$$ = new Subject<Observable<Action>>();
+const state$ = affect(affector, init, action$$);
+const dispatch = (action: Action) => {
+  action$$.next(of(action));
+};
 
-  componentDidMount() {
-    const scale = scaleLinear();
-    const axis = axisLeft(scale).tickSize(100);
-    select(this.svg.current)
-      .append("g")
-      .attr("transform", "translate(0,100)")
-      .call(axis);
+const { Provider, Consumer } = React.createContext({
+  dispatch,
+  state$
+});
 
-    console.log("scale", scale);
-    console.log("axis", axis);
-    // const axis = d3.axisLeft(scale);
-  }
+const connect = (
+  WrappedComponent: React.Component<any, any>,
+  mapState$ToProps?: Function
+) => {
+  return class Connect extends React.Component {
+    static displayName = `Wrapped${WrappedComponent.displayName}`;
+    static WrappedComponent = WrappedComponent;
+    constructor(props, context) {
+      super(props, context);
+      this.state = {};
+    }
+    render() {
+      return (
+        <Consumer>
+          {store => {
+            console.log(store);
+            return (
+              <WrappedComponent
+                state$={store.state$}
+                dispatch={dispatch}
+                {...this.props}
+              />
+            );
+          }}
+        </Consumer>
+      );
+    }
+  };
+};
 
-  render() {
-    return (
-      <div className="svg-container">
-        <svg
-          version="1.1"
-          viewBox="0 0 100 100"
-          preserveAspectRatio="xMinYMin meet"
-          className="svg-content"
-          transform="translate(0, -100)"
-        >
-          <svg
-            width="100"
-            height="100"
-            version="1.1"
-            className="svg-content"
-            ref={this.svg}
-          />
-        </svg>
-      </div>
-    );
-  }
-}
+const App = props => {
+  console.log(props);
+  return <h1>hi</h1>;
+};
 
-class App extends React.Component<AppProps, State> {
-  action$$: Subject<Observable<Action>>;
-  state$: Observable<State>;
-  sub: Subscription;
-  sub2: Subscription;
-  constructor(props: AppProps) {
-    super(props);
-
-    this.state = { username: null };
-  }
-
-  componentDidMount() {
-    this.action$$ = new Subject<Observable<Action>>();
-    this.state$ = affect(this.props.affector, this.props.init, this.action$$);
-    console.log("subscribing to state");
-    this.sub = this.state$.subscribe(state => {
-      console.log("got ur state", state);
-      this.setState(state);
-    });
-    console.log("subscribing to state again");
-    this.sub2 = this.state$.subscribe(state => {
-      console.log("got ur state again", state);
-    });
-    console.log("sending action");
-    this.action$$.next(this.props.action$);
-  }
-
-  componentWillUnmount() {
-    console.log(this.state$);
-
-    console.log("unsubscribing from componetn");
-    this.sub.unsubscribe();
-    this.sub2.unsubscribe();
-    console.log(this.state$);
-  }
-
-  public render() {
-    return <Heatmap />;
-  }
-}
+const WApp = connect(App);
 
 ReactDOM.render(
-  <App
-    affector={affector}
-    init={{ username: null }}
-    action$={of<Action>({
-      kind: "LoginRequest",
-      username: "supershabam",
-      password: "sekret"
-    })}
-  />,
+  <Provider value={{ dispatch, state$ }}>
+    <WApp />
+  </Provider>,
   document.getElementById("app")
 );
-
-setTimeout(() => {
-  ReactDOM.render(<h1>and it gone</h1>, document.getElementById("app"));
-}, 500);
